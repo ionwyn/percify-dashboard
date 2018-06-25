50b9d3278568645e0bf32a728567e366df68204c
Ionwyn Sean <ionwynsean@gmail.com>
2018-06-24 23:14:02 -0700

    configuration done

--- src/fuse-configs/fuseNavigationConfig.js
+++ src/fuse-configs/fuseNavigationConfig.js
@@ -1,17 +1,24 @@
 export const fuseNavigationConfig = [
-    {
-        'id'      : 'applications',
-        'title'   : 'Applications',
-        'type'    : 'group',
-        'icon'    : 'apps',
-        'children': [
-            {
-                'id'   : 'example-component',
-                'title': 'Example',
-                'type' : 'item',
-                'icon' : 'whatshot',
-                'url'  : '/example'
-            }
+  {
+    id: 'applications',
+    title: 'Applications',
+    type: 'group',
+    icon: 'apps',
+    children: [
+      {
+        id: 'dashboards',
+        title: 'Dashboards',
+        type: 'collapse',
+        icon: 'dashboard',
+        children: [
+          {
+            id: 'analytics-dashboard',
+            title: 'Analytics',
+            type: 'item',
+            url: '/apps/dashboards/analytics'
+          }
         ]
-    }
+      }
+    ]
+  }
 ];
--- src/fuse-configs/fuseRoutesConfig.js
+++ src/fuse-configs/fuseRoutesConfig.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import {Redirect} from 'react-router-dom';
-import {FuseUtils} from '@fuse/index';
-import {ExampleConfig} from 'main/content/example/ExampleConfig';
+import { Redirect } from 'react-router-dom';
+import { FuseUtils } from '@fuse/index';
+import { appsConfigs } from 'main/content/apps/appsConfigs';
+import { pagesConfigs } from 'main/content/pages/pagesConfigs';
+import { ExampleConfig } from 'main/content/example/ExampleConfig';
 
-const routeConfigs = [
-    ExampleConfig
-];
+const routeConfigs = [...appsConfigs, ...pagesConfigs];
 
 export const routes = [
-    ...FuseUtils.generateRoutesFromConfigs(routeConfigs),
-    {
-        path     : '/',
-        component: () => <Redirect to="/example"/>
-    }
+  ...FuseUtils.generateRoutesFromConfigs(routeConfigs),
+  {
+    path: '/',
+    component: () => <Redirect to="/apps/dashboards/analytics" />
+  }
 ];
