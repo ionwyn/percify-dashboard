f407bca2869bcf2e897c84f433cf56e95c9a5607
Ionwyn Sean <ionwynsean@gmail.com>
2018-06-24 23:15:31 -0700

    emulate actual use case

--- src/main/quickPanel/QuickPanel.js
+++ src/main/quickPanel/QuickPanel.js
@@ -1,47 +1,51 @@
-import React, {Component} from 'react';
-import {Drawer, Typography} from '@material-ui/core/';
-import {withStyles} from '@material-ui/core/styles/index';
-import {bindActionCreators} from 'redux';
-import {connect} from 'react-redux';
-import * as Actions from './store/actions/index'
+import React, { Component } from 'react';
+import { Drawer, Typography } from '@material-ui/core/';
+import { withStyles } from '@material-ui/core/styles/index';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as Actions from './store/actions/index';
 
 const styles = theme => ({
-    root: {
-        width  : 280,
-        padding: 24
-    }
+  root: {
+    width: 280,
+    padding: 24
+  }
 });
 
 class QuickPanel extends Component {
+  render() {
+    const { classes, state, toggleQuickPanel } = this.props;
+    return (
+      <Drawer
+        classes={{ paper: classes.root }}
+        open={state}
+        anchor="right"
+        onClose={() => toggleQuickPanel(false)}
+      >
+        <Typography>About MUSES</Typography>
+      </Drawer>
+    );
+  }
+}
 
-    render()
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
     {
-        const {classes, state, toggleQuickPanel} = this.props;
-        return (
-            <Drawer
-                classes={{paper: classes.root}}
-                open={state}
-                anchor="right"
-                onClose={() => toggleQuickPanel(false)}
-            >
-                <Typography>Quick Panel</Typography>
-            </Drawer>
-        );
-    }
+      toggleQuickPanel: Actions.toggleQuickPanel
+    },
+    dispatch
+  );
 }
 
-function mapDispatchToProps(dispatch)
-{
-    return bindActionCreators({
-        toggleQuickPanel: Actions.toggleQuickPanel
-    }, dispatch);
+function mapStateToProps({ quickPanel }) {
+  return {
+    state: quickPanel.state
+  };
 }
 
-function mapStateToProps({quickPanel})
-{
-    return {
-        state: quickPanel.state
-    }
-}
-
-export default withStyles(styles, {withTheme: true})(connect(mapStateToProps, mapDispatchToProps)(QuickPanel));
+export default withStyles(styles, { withTheme: true })(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(QuickPanel)
+);
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,12 +1,14 @@
-import {combineReducers} from 'redux';
+import { combineReducers } from 'redux';
 import fuse from './fuse';
 import auth from 'auth/store/reducers/index';
 import quickPanel from 'main/quickPanel/store/reducers';
+import analyticsDashboardApp from 'main/content/apps/dashboards/analytics/store/reducers/index';
 
 const rootReducer = combineReducers({
-    auth,
-    fuse,
-    quickPanel
+  auth,
+  fuse,
+  analyticsDashboardApp,
+  quickPanel
 });
 
 export default rootReducer;
