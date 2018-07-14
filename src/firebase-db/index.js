8403fb9f1d542fb66123e7641420a3c2c176bdad
Ionwyn Sean <isean@sfu.ca>
2018-07-14 12:08:41 -0700

    add firebase config

--- src/firebase-db/index.js
+++ src/firebase-db/index.js
@@ -3,37 +3,30 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const prodConfig = {
-    apiKey           : "YOUR_API_KEY",
-    authDomain       : "your-app.firebaseapp.com",
-    databaseURL      : "https://your-app.firebaseio.com",
-    projectId        : "your-app",
-    storageBucket    : "your-app.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID"
+  apiKey: 'AIzaSyC7ATI2v4QiWcWUePYlUmoMK4ZnkdE1c6Q',
+  authDomain: 'mayappca.firebaseapp.com ',
+  databaseURL: 'https://mayappca.firebaseio.com',
+  projectId: 'mayappca',
+  storageBucket: 'your-app.appspot.com',
+  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID'
 };
 
 const devConfig = {
-    apiKey           : "YOUR_API_KEY",
-    authDomain       : "your-app.firebaseapp.com",
-    databaseURL      : "https://your-app.firebaseio.com",
-    projectId        : "your-app",
-    storageBucket    : "your-app.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID"
+  apiKey: 'AIzaSyC7ATI2v4QiWcWUePYlUmoMK4ZnkdE1c6Q',
+  authDomain: 'mayappca.firebaseapp.com ',
+  databaseURL: 'https://mayappca.firebaseio.com',
+  projectId: 'mayappca',
+  storageBucket: 'your-app.appspot.com',
+  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID'
 };
 
-const config = process.env.NODE_ENV === 'production'
-    ? prodConfig
-    : devConfig;
+const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
 
-
-if ( !firebase.apps.length )
-{
-    firebase.initializeApp(config);
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
 }
 
 const db = firebase.database();
 const auth = firebase.auth();
 
-export {
-    auth,
-    db
-};
+export { auth, db };
