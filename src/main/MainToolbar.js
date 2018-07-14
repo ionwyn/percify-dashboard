0f17b32e45ec781cdc67c80f39e0fe71da692137
Ionwyn Sean <isean@sfu.ca>
2018-07-14 11:54:08 -0700

    add component for displaying authenticated user

--- src/main/MainToolbar.js
+++ src/main/MainToolbar.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles/index';
 import classNames from 'classnames';
-import { Icon, IconButton, Typography } from '@material-ui/core';
+import {
+  Avatar,
+  Button,
+  Icon,
+  IconButton,
+  ListItemIcon,
+  ListItemText,
+  Popover,
+  MenuItem,
+  Typography
+} from '@material-ui/core';
 import { connect } from 'react-redux';
 import * as quickPanelActions from 'main/quickPanel/store/actions';
+import * as authActions from 'auth/store/actions';
 import { bindActionCreators } from 'redux';
+import { FuseAnimate } from '@fuse';
+import { Link } from 'react-router-dom';
 
 const styles = theme => ({
   root: {
@@ -20,8 +33,21 @@ const styles = theme => ({
 });
 
 class MainToolbar extends Component {
+  state = {
+    userMenu: null
+  };
+
+  userMenuClick = event => {
+    this.setState({ userMenu: event.currentTarget });
+  };
+
+  userMenuClose = () => {
+    this.setState({ userMenu: null });
+  };
+
   render() {
-    const { classes, toggleQuickPanel } = this.props;
+    const { classes, toggleQuickPanel, user, logout } = this.props;
+    const { userMenu } = this.state;
 
     return (
       <div className={classNames(classes.root, 'flex flex-row')}>
@@ -30,6 +56,106 @@ class MainToolbar extends Component {
         </div>
 
         <div className="flex">
+          <FuseAnimate delay={300}>
+            <Button className="h-64" onClick={this.userMenuClick}>
+              {user.data.photoURL ? (
+                <Avatar
+                  className=""
+                  alt="user photo"
+                  src={user.data.photoURL}
+                />
+              ) : (
+                <Avatar className="">{user.data.displayName[0]}</Avatar>
+              )}
+
+              <div className="hidden md:flex flex-col ml-12 items-start">
+                <Typography
+                  component="span"
+                  className="normal-case font-500 flex"
+                >
+                  {user.data.displayName}
+                </Typography>
+                <Typography
+                  className="text-11 capitalize"
+                  color="textSecondary"
+                >
+                  {user.role}
+                </Typography>
+              </div>
+
+              <Icon className="text-16 ml-12 hidden sm:flex" variant="action">
+                keyboard_arrow_down
+              </Icon>
+            </Button>
+          </FuseAnimate>
+
+          <Popover
+            open={Boolean(userMenu)}
+            anchorEl={userMenu}
+            onClose={this.userMenuClose}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'center'
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'center'
+            }}
+            classes={{
+              paper: 'py-8'
+            }}
+          >
+            {user.role === 'guest' ? (
+              <React.Fragment>
+                <MenuItem component={Link} to="/login">
+                  <ListItemIcon>
+                    <Icon>lock</Icon>
+                  </ListItemIcon>
+                  <ListItemText className="pl-0" primary="Login" />
+                </MenuItem>
+                <MenuItem component={Link} to="/register">
+                  <ListItemIcon>
+                    <Icon>person_add</Icon>
+                  </ListItemIcon>
+                  <ListItemText className="pl-0" primary="Register" />
+                </MenuItem>
+              </React.Fragment>
+            ) : (
+              <React.Fragment>
+                <MenuItem
+                  component={Link}
+                  to="/pages/profile"
+                  onClick={this.userMenuClose}
+                >
+                  <ListItemIcon>
+                    <Icon>account_circle</Icon>
+                  </ListItemIcon>
+                  <ListItemText className="pl-0" primary="My Profile" />
+                </MenuItem>
+                <MenuItem
+                  component={Link}
+                  to="/apps/mail"
+                  onClick={this.userMenuClose}
+                >
+                  <ListItemIcon>
+                    <Icon>mail</Icon>
+                  </ListItemIcon>
+                  <ListItemText className="pl-0" primary="Inbox" />
+                </MenuItem>
+                <MenuItem
+                  onClick={() => {
+                    logout();
+                    this.userMenuClose();
+                  }}
+                >
+                  <ListItemIcon>
+                    <Icon>exit_to_app</Icon>
+                  </ListItemIcon>
+                  <ListItemText className="pl-0" primary="Logout" />
+                </MenuItem>
+              </React.Fragment>
+            )}
+          </Popover>
           <div className={classes.seperator} />
 
           <IconButton
@@ -47,15 +173,22 @@ class MainToolbar extends Component {
 function mapDispatchToProps(dispatch) {
   return bindActionCreators(
     {
-      toggleQuickPanel: quickPanelActions.toggleQuickPanel
+      toggleQuickPanel: quickPanelActions.toggleQuickPanel,
+      logout: authActions.logoutUser
     },
     dispatch
   );
 }
 
+function mapStateToProps({ auth }) {
+  return {
+    user: auth.user
+  };
+}
+
 export default withStyles(styles, { withTheme: true })(
   connect(
-    null,
+    mapStateToProps,
     mapDispatchToProps
   )(MainToolbar)
 );
