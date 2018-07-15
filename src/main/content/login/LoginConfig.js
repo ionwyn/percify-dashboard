b353c61aa64aae6d31f3d88bf7ae3fa3af2c1e9a
Unknown <isean@sfu.ca>
2018-07-15 01:48:17 -0700

    add login component and config

---
+++ src/main/content/login/LoginConfig.js
@@ -0,0 +1,27 @@
+import Login from './Login';
+import { authRoles } from 'auth/auth';
+
+export const LoginConfig = {
+  settings: {
+    layout: {
+      config: {
+        navbar: {
+          display: false
+        },
+        toolbar: {
+          display: false
+        },
+        footer: {
+          display: false
+        }
+      }
+    }
+  },
+  auth: authRoles.onlyGuest,
+  routes: [
+    {
+      path: '/login',
+      component: Login
+    }
+  ]
+};
