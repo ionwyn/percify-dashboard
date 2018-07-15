a9ad34aa47f3c14b00a10aad76363f890de676f4
Unknown <isean@sfu.ca>
2018-07-15 01:50:11 -0700

    add loginConfig, registerConfig, logoutConfig, and authRoleConfig

--- src/fuse-configs/fuseRoutesConfig.js
+++ src/fuse-configs/fuseRoutesConfig.js
@@ -3,13 +3,24 @@ import { Redirect } from 'react-router-dom';
 import { FuseUtils } from '@fuse/index';
 import { appsConfigs } from 'main/content/apps/appsConfigs';
 import { pagesConfigs } from 'main/content/pages/pagesConfigs';
+import { authRoleExamplesConfigs } from 'main/content/auth/authRoleExamplesConfigs';
+import { LoginConfig } from 'main/content/login/LoginConfig';
+import { RegisterConfig } from 'main/content/register/RegisterConfig';
+import { LogoutConfig } from 'main/content/logout/LogoutConfig';
 
-const routeConfigs = [...appsConfigs, ...pagesConfigs];
+const routeConfigs = [
+  ...appsConfigs,
+  ...pagesConfigs,
+  ...authRoleExamplesConfigs,
+  LoginConfig,
+  RegisterConfig,
+  LogoutConfig
+];
 
 export const routes = [
   ...FuseUtils.generateRoutesFromConfigs(routeConfigs),
   {
     path: '/',
-    component: () => <Redirect to="/apps/dashboards/analytics" />
+    component: () => <Redirect to="/login" />
   }
 ];
