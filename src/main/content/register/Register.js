4633fb4a05c77e7ebe60d533624aca354752cb32
Unknown <isean@sfu.ca>
2018-07-15 01:48:00 -0700

    add register component and config

---
+++ src/main/content/register/Register.js
@@ -0,0 +1,240 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as Actions from 'auth/store/actions/index';
+import { bindActionCreators } from 'redux';
+import { Link, withRouter } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles/index';
+import { Button, Card, CardContent, Typography } from '@material-ui/core';
+import classNames from 'classnames';
+import { TextFieldFormsy } from '@fuse';
+import Formsy from 'formsy-react';
+import { FuseAnimate } from '@fuse';
+
+const styles = theme => ({
+  root: {
+    background:
+      "url('/assets/images/backgrounds/dark-material-bg.jpg') no-repeat",
+    backgroundSize: 'cover'
+  },
+  intro: {
+    color: '#ffffff'
+  },
+  card: {
+    width: '100%',
+    maxWidth: 400
+  }
+});
+
+class Register extends Component {
+  state = {
+    canSubmit: false
+  };
+
+  form = React.createRef();
+
+  disableButton = () => {
+    this.setState({ canSubmit: false });
+  };
+
+  enableButton = () => {
+    this.setState({ canSubmit: true });
+  };
+
+  onSubmit = model => {
+    this.props.registerWithFirebase(model);
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      this.props.login.error &&
+      (this.props.login.error.displayName ||
+        this.props.login.error.password ||
+        this.props.login.error.email)
+    ) {
+      this.form.updateInputsWithError({
+        ...this.props.login.error
+      });
+
+      this.props.login.error = null;
+      this.disableButton();
+    }
+
+    if (this.props.user.role !== 'guest') {
+      const pathname =
+        this.props.location.state && this.props.location.state.redirectUrl
+          ? this.props.location.state.redirectUrl
+          : '/';
+      this.props.history.push({
+        pathname
+      });
+    }
+    return null;
+  }
+
+  render() {
+    const { classes } = this.props;
+    const { canSubmit } = this.state;
+
+    return (
+      <div
+        className={classNames(
+          classes.root,
+          'flex flex-col flex-1 flex-no-shrink p-24 md:flex-row md:p-0'
+        )}
+      >
+        <div
+          className={classNames(
+            classes.intro,
+            'flex flex-col flex-no-grow items-center p-16 text-center md:p-128 md:items-start md:flex-no-shrink md:flex-1 md:text-left'
+          )}
+        >
+          <FuseAnimate animation="transition.expandIn">
+            <img
+              className="w-128 mb-32"
+              src="assets/images/logos/fuse.svg"
+              alt="logo"
+            />
+          </FuseAnimate>
+
+          <FuseAnimate animation="transition.slideUpIn" delay={300}>
+            <Typography
+              variant="display2"
+              color="inherit"
+              className="font-light"
+            >
+              Welcome to the FUSE!
+            </Typography>
+          </FuseAnimate>
+
+          <FuseAnimate delay={400}>
+            <Typography
+              variant="subheading"
+              color="inherit"
+              className="max-w-512 mt-16"
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+              ullamcorper nisl erat, vel convallis elit fermentum pellentesque.
+              Sed mollis velit facilisis facilisis.
+            </Typography>
+          </FuseAnimate>
+        </div>
+
+        <FuseAnimate animation={{ translateX: [0, '100%'] }}>
+          <Card className={classNames(classes.card, 'mx-auto m-16 md:m-0')}>
+            <CardContent className="flex flex-col items-center justify-center p-32 md:p-48 md:pt-128 ">
+              <Typography variant="title" className="md:w-full mb-32">
+                CREATE AN ACCOUNT
+              </Typography>
+
+              <Formsy
+                onValidSubmit={this.onSubmit}
+                onValid={this.enableButton}
+                onInvalid={this.disableButton}
+                ref={form => (this.form = form)}
+                className="flex flex-col justify-center w-full"
+              >
+                <TextFieldFormsy
+                  className="mb-16"
+                  type="text"
+                  name="displayName"
+                  label="Display name"
+                  validations={{
+                    minLength: 4
+                  }}
+                  validationErrors={{
+                    minLength: 'Min character length is 4'
+                  }}
+                  required
+                />
+
+                <TextFieldFormsy
+                  className="mb-16"
+                  type="text"
+                  name="email"
+                  label="Email"
+                  validations="isEmail"
+                  validationErrors={{
+                    isEmail: 'Please enter a valid email'
+                  }}
+                  required
+                />
+
+                <TextFieldFormsy
+                  className="mb-16"
+                  type="password"
+                  name="password"
+                  label="Password"
+                  validations="equalsField:password-confirm"
+                  validationErrors={{
+                    equalsField: 'Passwords do not match'
+                  }}
+                  required
+                />
+
+                <TextFieldFormsy
+                  className="mb-16"
+                  type="password"
+                  name="password-confirm"
+                  label="Confirm Password"
+                  validations="equalsField:password"
+                  validationErrors={{
+                    equalsField: 'Passwords do not match'
+                  }}
+                  required
+                />
+
+                <Button
+                  type="submit"
+                  variant="raised"
+                  color="primary"
+                  className="w-full mx-auto mt-16 normal-case"
+                  aria-label="REGISTER WITH FIREBASE"
+                  disabled={!canSubmit}
+                >
+                  Register with Firebase
+                </Button>
+              </Formsy>
+
+              <div className="flex flex-col items-center justify-center pt-32 pb-24">
+                <span className="font-medium">Already have an account?</span>
+                <Link className="font-medium" to="/login">
+                  Login
+                </Link>
+                <Link className="font-medium mt-8" to="/">
+                  Back to Dashboard
+                </Link>
+              </div>
+
+              <div className="flex flex-col items-center" />
+            </CardContent>
+          </Card>
+        </FuseAnimate>
+      </div>
+    );
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    {
+      registerWithFirebase: Actions.registerWithFirebase
+    },
+    dispatch
+  );
+}
+
+function mapStateToProps({ auth }) {
+  return {
+    login: auth.login,
+    user: auth.user
+  };
+}
+
+export default withStyles(styles, { withTheme: true })(
+  withRouter(
+    connect(
+      mapStateToProps,
+      mapDispatchToProps
+    )(Register)
+  )
+);
---
+++ src/main/content/register/RegisterConfig.js
@@ -0,0 +1,27 @@
+import { authRoles } from 'auth/auth';
+import Register from './Register';
+
+export const RegisterConfig = {
+  settings: {
+    layout: {
+      config: {
+        navbar: {
+          display: false
+        },
+        toolbar: {
+          display: false
+        },
+        footer: {
+          display: false
+        }
+      }
+    }
+  },
+  auth: authRoles.onlyGuest,
+  routes: [
+    {
+      path: '/register',
+      component: Register
+    }
+  ]
+};
