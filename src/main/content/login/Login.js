fea952204c3a14a505b3cfeaa3a9a6da8f2a1d23
Unknown <isean@sfu.ca>
2018-07-15 01:48:28 -0700

    add login component

---
+++ src/main/content/login/Login.js
@@ -0,0 +1,160 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as Actions from 'auth/store/actions/index';
+import { bindActionCreators } from 'redux';
+import { withRouter } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles/index';
+import { Button, Card, CardContent, Typography } from '@material-ui/core';
+import classNames from 'classnames';
+import { FuseAnimate } from '@fuse';
+
+const styles = theme => ({
+  root: {
+    background:
+      "url('/assets/images/backgrounds/dark-material-bg.jpg') no-repeat",
+    backgroundSize: 'cover'
+  },
+  intro: {
+    color: '#ffffff'
+  },
+  card: {
+    width: '100%',
+    maxWidth: 400
+  }
+});
+
+let scopes = 'user-read-private user-read-email';
+let my_client_id = 'd104e370c7a1420aba8892389177ccb0';
+
+const spotifyRedirect = 'https://google.com';
+
+class Login extends Component {
+  onSubmit = model => {
+    this.props.submitLogin(model);
+  };
+
+  loginWithFireBase = () => {
+    const model = this.form.getModel();
+    this.props.loginWithFireBase(model);
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      this.props.login.error &&
+      (this.props.login.error.username || this.props.login.error.password)
+    ) {
+      this.form.updateInputsWithError({
+        ...this.props.login.error
+      });
+
+      this.props.login.error = null;
+      this.disableButton();
+    }
+
+    if (this.props.user.role !== 'guest') {
+      const pathname =
+        this.props.location.state && this.props.location.state.redirectUrl
+          ? this.props.location.state.redirectUrl
+          : '/';
+      this.props.history.push({
+        pathname
+      });
+    }
+    return null;
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div
+        className={classNames(
+          classes.root,
+          'flex flex-col flex-1 flex-no-shrink p-24 md:flex-row md:p-0'
+        )}
+      >
+        <div
+          className={classNames(
+            classes.intro,
+            'flex flex-col flex-no-grow items-center p-16 text-center md:p-128 md:items-start md:flex-no-shrink md:flex-1 md:text-left'
+          )}
+        >
+          <FuseAnimate animation="transition.expandIn">
+            <img
+              className="w-128 mb-32"
+              src="assets/images/logos/fuse.svg"
+              alt="logo"
+            />
+          </FuseAnimate>
+
+          <FuseAnimate animation="transition.slideUpIn" delay={300}>
+            <Typography
+              variant="display2"
+              color="inherit"
+              className="font-light"
+            >
+              Welcome to Intersect!
+            </Typography>
+          </FuseAnimate>
+
+          <FuseAnimate delay={400}>
+            <Typography
+              variant="subheading"
+              color="inherit"
+              className="max-w-512 mt-16"
+            >
+              Know more about how you listen.
+            </Typography>
+          </FuseAnimate>
+        </div>
+
+        <FuseAnimate animation={{ translateX: [0, '100%'] }}>
+          <Card className={classNames(classes.card, 'mx-auto m-16 md:m-0')}>
+            <CardContent className="flex flex-col items-center justify-center p-32 md:p-48 md:pt-128 ">
+              <Button
+                className="px-4"
+                component="a"
+                href={spotifyRedirect}
+                target="_blank"
+                rel="noreferrer noopener"
+                fullWidth={true}
+                variant="flat"
+              >
+                <img
+                  src="assets/images/logos/spotifylogin.svg"
+                  alt="spotifyLogin"
+                />
+              </Button>
+            </CardContent>
+          </Card>
+        </FuseAnimate>
+      </div>
+    );
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    {
+      submitLogin: Actions.submitLogin,
+      loginWithFireBase: Actions.loginWithFireBase
+    },
+    dispatch
+  );
+}
+
+function mapStateToProps({ auth }) {
+  return {
+    login: auth.login,
+    user: auth.user
+  };
+}
+
+export default withStyles(styles, { withTheme: true })(
+  withRouter(
+    connect(
+      mapStateToProps,
+      mapDispatchToProps
+    )(Login)
+  )
+);
