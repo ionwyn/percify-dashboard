ee2a91c334cd6aac362ca95fa44793c3a94d8879
Unknown <isean@sfu.ca>
2018-07-24 00:26:30 -0700

    Change default theming

--- src/fuse-configs/fuseSettingsConfig.js
+++ src/fuse-configs/fuseSettingsConfig.js
@@ -1,33 +1,33 @@
 const fuseSettingsConfig = {
-    layout          : {
-        style : 'layout1',
-        config: {
-            scroll : 'content',
-            navbar : {
-                display : true,
-                folded  : false,
-                position: 'left'
-            },
-            toolbar: {
-                display : true,
-                style   : 'fixed',
-                position: 'below'
-            },
-            footer : {
-                display : true,
-                style   : 'fixed',
-                position: 'below'
-            },
-            mode   : 'fullwidth'
-        }
-    },
-    customScrollbars: true,
-    theme           : {
-        main   : 'default',
-        navbar : 'mainThemeDark',
-        toolbar: 'mainThemeLight',
-        footer : 'mainThemeDark'
+  layout: {
+    style: 'layout1',
+    config: {
+      scroll: 'content',
+      navbar: {
+        display: true,
+        folded: true,
+        position: 'left'
+      },
+      toolbar: {
+        display: true,
+        style: 'fixed',
+        position: 'below'
+      },
+      footer: {
+        display: false,
+        style: 'fixed',
+        position: 'below'
+      },
+      mode: 'fullwidth'
     }
+  },
+  customScrollbars: true,
+  theme: {
+    main: 'default',
+    navbar: 'mainThemeDark',
+    toolbar: 'mainThemeLight',
+    footer: 'mainThemeDark'
+  }
 };
 
 export default fuseSettingsConfig;
