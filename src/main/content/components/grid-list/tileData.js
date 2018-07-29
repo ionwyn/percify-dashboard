57404cce3c518cb5cdd3e9513f463cd773eba368
Unknown <isean@sfu.ca>
2018-07-29 04:40:20 -0700

    Create component grid list

---
+++ src/main/content/components/grid-list/tileData.js
@@ -0,0 +1,43 @@
+const tileData = [
+  {
+    img: 'https://i.scdn.co/image/b4d28bb35c668b6c4617aba68234db52e81e32ea',
+    title: 'Sweet Disposition',
+    author: 'The Temper Trap',
+    cols: 2,
+    featured: true
+  },
+  {
+    img: 'https://i.scdn.co/image/a4afdd483397de5807e404076087ffd2c06f3ad1',
+    title: 'Sunlight',
+    author: 'The Magician'
+  },
+  {
+    img: 'https://i.scdn.co/image/9c8cd5bb09ed0f9be3a477a65d53d6d94fd76ee8',
+    title: 'Gecko',
+    author: 'Oliver Heldens'
+  },
+  {
+    img: 'https://i.scdn.co/image/52ac4c5a82658db3e57a5ac20a2f78b8ae823ec6',
+    title: 'Come Back',
+    author: 'Tom Misch',
+    featured: true
+  },
+  {
+    img: 'https://i.scdn.co/image/e285111c1c501deb989adb696a5c398eb0ae496a',
+    title: 'Best Friend',
+    author: 'Sofi Tukker'
+  },
+  {
+    img: 'https://i.scdn.co/image/5434a3f4cb2b2bae89a92c9d59a921efef1d6058',
+    title: 'Around the World / Harder, Better, Faster, Stronger',
+    author: 'Daft Punk'
+  },
+  {
+    img: 'https://i.scdn.co/image/3b5f5d472ca5ba120051362461499bc8541c19fe',
+    title: 'First Class',
+    author: 'Henry Jackman',
+    cols: 2
+  }
+];
+
+export default tileData;
