23f0d6d2301b9d4fc7f7a1bf3f19957af442eedf
Unknown <isean@sfu.ca>
2018-07-29 04:40:54 -0700

    pass user top metrics to root reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -3,12 +3,14 @@ import fuse from './fuse';
 import auth from 'auth/store/reducers/index';
 import quickPanel from 'main/quickPanel/store/reducers';
 import analyticsDashboardApp from 'main/content/apps/dashboards/analytics/store/reducers/index';
+import userTop from 'main/content/apps/home/store/reducers/index';
 
 const rootReducer = combineReducers({
   auth,
   fuse,
   analyticsDashboardApp,
-  quickPanel
+  quickPanel,
+  userTop
 });
 
 export default rootReducer;
