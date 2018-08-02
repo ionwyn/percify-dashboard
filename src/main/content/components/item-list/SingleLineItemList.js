ae7a129cd9ee1871e403a1cae3af3e9d7ec19377
Unknown <isean@sfu.ca>
2018-08-02 00:22:41 -0700

    Create vertical itemList for possible migration from GridList
    We might use itemList instead of Gridlist, for displaying tracks and artists

---
+++ src/main/content/components/item-list/SingleLineItemList.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper
+  },
+  gridList: {
+    flexWrap: 'nowrap',
+    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+    transform: 'translateZ(0)'
+  },
+  title: {
+    color: theme.palette.primary.light
+  },
+  titleBar: {
+    background:
+      'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)'
+  }
+});
+
+/**
+ * The example data is structured as follows:
+ *
+ * import image from 'path/to/image.jpg';
+ * [etc...]
+ *
+ * const tileData = [
+ *   {
+ *     img: image,
+ *     title: 'Image',
+ *     author: 'author',
+ *   },
+ *   {
+ *     [etc...]
+ *   },
+ * ];
+ */
+function SingleLineGridList(props) {
+  const { classes, userTop } = props;
+
+  return (
+    <div className={classes.root}>
+      <List className={classes.gridList} cols={2.5}>
+        {userTop.map(tile => (
+          <ListItem key={tile.uri}>
+            <img src={tile.img} alt={tile.name} />
+            <ListItemText primary={tile.name} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+}
+
+SingleLineGridList.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(SingleLineGridList);
