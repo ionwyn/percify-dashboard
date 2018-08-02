2de8dcf658fd2729888a44aea73ee2f334e6d657
Unknown <isean@sfu.ca>
2018-08-01 23:39:51 -0700

    remove playback redux stuff as not needed

--- src/main/content/pages/playback/store/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import playbackr from './playback.reducer';
-
-const playbackReducer = combineReducers({
-  playbackr
-});
-
-export default playbackReducer;
