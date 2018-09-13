2befd4f5535d5695697c171694563fd177efd821
Ionwyn Sean <isean@sfu.ca>
2018-09-13 10:40:44 -0700

    change theme

--- src/fuse-configs/fuseSettingsConfig.js
+++ src/fuse-configs/fuseSettingsConfig.js
@@ -23,7 +23,7 @@ const fuseSettingsConfig = {
   },
   customScrollbars: true,
   theme: {
-    main: 'slate',
+    main: 'defaultDark',
     navbar: 'mainThemeDark',
     toolbar: 'mainThemeDark',
     footer: 'mainThemeDark'
